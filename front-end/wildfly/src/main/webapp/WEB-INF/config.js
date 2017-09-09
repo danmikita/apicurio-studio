f106819416e95f00a150f7f5aaeb5187bef5e466
Dan Mikita <dan.mikita@spectrumhealth.org>
2017-09-09 07:23:59 -0400

    Most recent work

---
+++ front-end/wildfly/src/main/webapp/WEB-INF/config.js
@@ -0,0 +1,10 @@
+var ApicurioStudioConfig = {
+    mode: "dev",
+    auth: {
+        type: "keycloakjs"
+    },
+    apis: {
+        type: "hub",
+        hubUrl: "http://apicurio-studio-be-hub-api/api-hub"
+    }
+};
