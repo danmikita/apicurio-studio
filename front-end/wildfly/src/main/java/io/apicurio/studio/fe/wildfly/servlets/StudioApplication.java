0d8992e419ce1173596e15734bad90c6872cf138
Dan Mikita <dan.mikita@spectrumhealth.org>
2017-09-09 08:54:43 -0400

    Fixed

---
+++ front-end/wildfly/src/main/java/io/apicurio/studio/fe/wildfly/servlets/StudioApplication.java
@@ -0,0 +1,10 @@
+package io.apicurio.studio.fe.wildfly.servlets;
+
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("studio")
+public class StudioApplication extends Application {
+    // Left empty intentionally
+}
